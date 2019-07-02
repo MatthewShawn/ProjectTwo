be8ff4b21cc978db20b19cd710b61da6b6d3412f
pando <spreadpando@gmail.com>
2019-07-01 21:31:13 -0600

    login.js refactored to vue

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const LogIn = Vue.component("LogIn", {
       <div class="row">
         <div class="col-md-6 col-md-offset-3">
           <h2>Login Form</h2>
-          <form class="login">
+          <form class="login" v-on:submit.prevent="logIn()">
             <div class="form-group">
               <label for="exampleInputEmail1">User Name</label>
               <input
@@ -44,51 +44,41 @@ const LogIn = Vue.component("LogIn", {
     </div>
   `,
 	data: {},
-	methods: {}
+	methods: {
+		logIn() {
+			var textInput = $("input#text-input");
+			var passwordInput = $("input#password-input");
+			var userData = {
+				text: textInput.val().trim(),
+				password: passwordInput.val().trim()
+			};
+			if (!userData.text || !userData.password) {
+				return;
+			}
+			// If we have an text and password we run the loginUser function and clear the form
+			this.$root.loginUser(userData.text, userData.password);
+			textInput.val("");
+			passwordInput.val("");
+		}
+	}
 });
 
 const Vapp = new Vue({
 	el: "#app",
 	data: {},
-	methods: {}
-});
-
-$(document).ready(function() {
-	// Getting references to our form and inputs
-	var loginForm = $("form.login");
-	var textInput = $("input#text-input");
-	var passwordInput = $("input#password-input");
-
-	// When the form is submitted, we validate there's an text and password entered
-	loginForm.on("submit", function(event) {
-		event.preventDefault();
-		var userData = {
-			text: textInput.val().trim(),
-			password: passwordInput.val().trim()
-		};
-
-		if (!userData.text || !userData.password) {
-			return;
-		}
-
-		// If we have an text and password we run the loginUser function and clear the form
-		loginUser(userData.text, userData.password);
-		textInput.val("");
-		passwordInput.val("");
-	});
-
-	// loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-	function loginUser(text, password) {
-		$.post("/api/login", {
-			text: text,
-			password: password
-		})
-			.then(function() {
-				window.location.replace("/members");
-				// If there's an error, log the error
+	methods: {
+		loginUser(text, password) {
+			$.post("/api/login", {
+				text: text,
+				password: password
 			})
-			.catch(function(err) {
-				console.log(err);
-			});
+				.then(function() {
+					window.location.replace("/members");
+					// If there's an error, log the error
+				})
+				.catch(function(err) {
+					console.log(err);
+				});
+		}
 	}
 });
