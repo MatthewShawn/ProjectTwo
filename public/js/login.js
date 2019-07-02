7b2999e7ac1d6f413f609e5b67f14fa287bd3120
pando <spreadpando@gmail.com>
2019-07-01 20:20:49 -0600

    vue rendering

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,94 @@
+const Top = Vue.component("Top", {
+	template: `
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header"></div>
+    </div>
+  </nav>
+  `,
+	data: {},
+	methods: {}
+});
+
+const LogIn = Vue.component("LogIn", {
+	template: `
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <h2>Login Form</h2>
+          <form class="login">
+            <div class="form-group">
+              <label for="exampleInputEmail1">User Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="text-input"
+                placeholder="User Name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password-input"
+                placeholder="Password"
+              />
+            </div>
+            <button type="submit" class="btn btn-default">Login</button>
+          </form>
+          <br />
+          <p>Or sign up <a href="/">here</a></p>
+        </div>
+      </div>
+    </div>
+  `,
+	data: {},
+	methods: {}
+});
+
+const Vapp = new Vue({
+	el: "#app",
+	data: {},
+	methods: {}
+});
+
 $(document).ready(function() {
-  // Getting references to our form and inputs
-  var loginForm = $("form.login");
-  var textInput = $("input#text-input");
-  var passwordInput = $("input#password-input");
+	// Getting references to our form and inputs
+	var loginForm = $("form.login");
+	var textInput = $("input#text-input");
+	var passwordInput = $("input#password-input");
 
-  // When the form is submitted, we validate there's an text and password entered
-  loginForm.on("submit", function(event) {
-    event.preventDefault();
-    var userData = {
-      text: textInput.val().trim(),
-      password: passwordInput.val().trim()
-    };
+	// When the form is submitted, we validate there's an text and password entered
+	loginForm.on("submit", function(event) {
+		event.preventDefault();
+		var userData = {
+			text: textInput.val().trim(),
+			password: passwordInput.val().trim()
+		};
 
-    if (!userData.text || !userData.password) {
-      return;
-    }
+		if (!userData.text || !userData.password) {
+			return;
+		}
 
-    // If we have an text and password we run the loginUser function and clear the form
-    loginUser(userData.text, userData.password);
-    textInput.val("");
-    passwordInput.val("");
-  });
+		// If we have an text and password we run the loginUser function and clear the form
+		loginUser(userData.text, userData.password);
+		textInput.val("");
+		passwordInput.val("");
+	});
 
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(text, password) {
-    $.post("/api/login", {
-      text: text,
-      password: password
-    })
-      .then(function() {
-        window.location.replace("/members");
-        // If there's an error, log the error
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-  }
+	// loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+	function loginUser(text, password) {
+		$.post("/api/login", {
+			text: text,
+			password: password
+		})
+			.then(function() {
+				window.location.replace("/members");
+				// If there's an error, log the error
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+	}
 });
--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,43 @@
+const Top = Vue.component("Top", {
+	template: `
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/logout">
+          Logout
+        </a>
+      </div>
+    </div>
+  </nav>
+  `,
+	data: {},
+	methods: {}
+});
+
+const Welcome = Vue.component("Welcome", {
+	template: `
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h2>Welcome <span class="member-name"></span></h2>
+      </div>
+    </div>
+  </div>
+  `,
+	data: {},
+	methods: {}
+});
+
+const Vapp = new Vue({
+	el: "#app",
+	data: {},
+	methods: {}
+});
+//--------------------------- Refactor this to Vue methods ---------------
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/employees_data").then(function(data) {
-    $(".member-name").text(data.text);
-  });
+	// This file just does a GET request to figure out which user is logged in
+	// and updates the HTML on the page
+	$.get("/api/employees_data").then(function(data) {
+		$(".member-name").text(data.text);
+	});
 });
