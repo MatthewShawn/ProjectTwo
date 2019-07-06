9a012c88da18095615bf9308c5265e81f41342a8
RC Cola <41647118+Royal-Crown-Cola@users.noreply.github.com>
2019-07-06 08:53:36 -0600

    updated survey page

--- models/emp_skills.js
+++ models/emp_skills.js
@@ -1,33 +1,35 @@
 module.exports = function(sequelize, DataTypes) {
-    var Emp_skills = sequelize.define(
-        "Emp_skills", {
-            current_level: {
-                type: DataTypes.INTEGER,
-                allowNull: true,
-                len: [1]
-            }
-        }, {
-            freezeTableName: true,
-            underscored: true
-        }
-    );
+  var Emp_skills = sequelize.define(
+    "Emp_skills",
+    {
+      current_level: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        len: [1]
+      }
+    },
+    {
+      freezeTableName: true,
+      underscored: true
+    }
+  );
 
-    Emp_skills.associate = function(models) {
-        // We're saying that an Employee skill belongs to an Employee at a specific
-        // level, and that it must have a skill data defined that can exist for other
-        // Emp_skills and Job_skills
+  Emp_skills.associate = function(models) {
+    // We're saying that an Employee skill belongs to an Employee at a specific
+    // level, and that it must have a skill data defined that can exist for other
+    // Emp_skills and Job_skills
 
-        Emp_skills.belongsTo(models.Employees, {
-            foreignKey: "id",
-            as: "employees_id",
-            allowNull: false
-        });
-        Emp_skills.belongsTo(models.Skill_data, {
-            foreignKey: "id",
-            as: "skill_data_id",
-            allowNull: false
-        });
-    };
+    Emp_skills.belongsTo(models.Employees, {
+      foreignKey: "id",
+      as: "employees_id",
+      allowNull: false
+    });
+    Emp_skills.belongsTo(models.Skill_data, {
+      foreignKey: "id",
+      as: "skill_data_id",
+      allowNull: false
+    });
+  };
 
-    return Emp_skills;
+  return Emp_skills;
 };
