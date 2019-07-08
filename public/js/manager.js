6ae47eade1f3ee8578c4c751ab13a2f7b17feb3f
Will <wfritz5@gmail.com>
2019-07-07 22:41:43 -0600

    display role id not roles_title

--- public/js/manager.js
+++ public/js/manager.js
@@ -34,8 +34,9 @@ const Welcome = Vue.component("Welcome", {
         <table class="table table-striped">
         <thead>
           <tr>
-            <th>Name</th>
+						<th>Name</th>
             <th>Edit Role Info</th>
+						<th>Current Role</th>
 						<th>Review</th>
 						<th>Salary</th>
 						<th>Score</th>
@@ -63,7 +64,7 @@ const Vapp = new Vue({
 	methods: {}
 });
 //--------------------------- Refactor this to Vue methods ---------------
-$(document).ready(function() {
+$(document).ready(function () {
 	// This file just does a GET request to figure out which user is logged in
 	// and updates the HTML on the page
 	let employeeNameInput = $("#employee-name");
@@ -84,9 +85,9 @@ $(document).ready(function() {
 
 		if (
 			!employeeNameInput
-				.val()
-				.trim()
-				.trim()
+			.val()
+			.trim()
+			.trim()
 		) {
 			return;
 		}
@@ -118,12 +119,14 @@ $(document).ready(function() {
 				`<td><select id="first-choice">
 			<option selected value="base">Please Select</option>` + optionsArray.toString()
 			);
+			newTr.append(`<td>${employeeData.role_id}</td>`);
+			console.log(employeeRole);
 			newTr.append("</select></td>");
 		}
 		newTr.append(
 			"<td><a href='/survey?employee_id=" +
-				employeeData.id +
-				"'>Create a Review</a></td>"
+			employeeData.id +
+			"'>Create a Review</a></td>"
 		);
 		newTr.data("employees", employeeData);
 		newTr.append(`<td>${employeeData.salary}</td>`);
@@ -143,10 +146,10 @@ $(document).ready(function() {
 	function getEmployees() {
 		let empData;
 		let rowsToAdd = [];
-		$.get("api/employees", function(data) {
+		$.get("api/employees", function (data) {
 			empData = data;
-		}).then(function(empData) {
-			$.get("api/role", function(roles) {
+		}).then(function (empData) {
+			$.get("api/role", function (roles) {
 				for (var i = 0; i < empData.length; i++) {
 					rowsToAdd.push(createEmployeeRow(empData[i], roles));
 				}
@@ -202,7 +205,7 @@ $(document).ready(function() {
 			method: "PUT",
 			url: "/api/employees",
 			data: request,
-			success: function(result) {
+			success: function (result) {
 				console.log(result);
 			}
 		});
